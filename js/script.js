8a9b707d2a3760321b8759e79315c4fb1e0e626d
Elvin <elvinabaev@gmail.com>
2023-07-11 02:51:50 +0300

    Apply conditions and loops in application.

--- js/script.js
+++ js/script.js
@@ -3,20 +3,34 @@
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = +prompt("На сколько оцените его?", "");
 
-    
-personalMovieDB[a] = b;
-personalMovieDB[c] = d;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB[a] = b;
+    console.log("done");
+  } else {
+    console.error();
+    i--;
+  }
+}
+
+if (personalMovieDB["count"] < 10) {
+  console.log("Просмотрено довольно мало фильмов...");
+} else if (personalMovieDB["count"] >= 10 && personalMovieDB["count"] <= 30) {
+  console.log("Вы - классический зритель");
+} else if (personalMovieDB["count"] >= 30) {
+  console.log("Вы - киноман");
+} else {
+  console.error();
+}
 
 console.log(personalMovieDB);
